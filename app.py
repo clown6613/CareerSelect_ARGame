0a6b454e6321d52ca9fc500684fada76aad4cf39
clown6613 <clown.66137@gmail.com>
2022-05-28 14:41:28 +0900

    fix code

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def getpoint():
-    return "144.44455.22"\
-        "14456"
-    
+    return render_template('index.html')
+
 @app.route('/result',methods=['POST'])
 def result():
     name = request.form['name']
